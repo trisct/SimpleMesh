6e7fb8d5a261c244baa57ec066fc68682d8ad331
trisct <29689546+trisct@users.noreply.github.com>
2021-04-08 17:14:20 +0800

    new file:   .gitignore 	modified:   README.md 	modified:   simplemesh.py

--- simplemesh.py
+++ simplemesh.py
@@ -7,26 +7,28 @@ import einops
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', type=str, required=True)
 parser.add_argument('-I', '--info', action='store_true')
-parser.add_argument('-o', '--output', type=str)
+parser.add_argument('-r', '--rotate', nargs='+')
 parser.add_argument('-n', '--normalize', type=float)
-parser.add_argument('-r', '--rotate', nargs=2)
-parser.add_argument('--skip_textures', action='store_true')
+parser.add_argument('-o', '--output', nargs='?', const=True)
 
 pi = math.pi
-opt = parser.parse_args()
+args = parser.parse_args()
 
 #skip_textures = True if opt.skip_textures else False
 
-mesh = trimesh.load(opt.input)#, skip_textures=skip_textures)
-
-if opt.info:
-    print('[HERE: In simplemesh] --info specified, displaying object info...')
+def display_info(mesh):
+    print('[In simplemesh] --info specified, displaying object info...')
     print(f'| Object type: {mesh}')
-    print(f'| Object bbox: {mesh.bounds}')
+    print(f'| Object bbox:\n{mesh.bounds}')
 
 
-if opt.normalize is not None:
-    print('[HERE: In simplemesh] --normalize specified, target bounding box is [-%.4f, %.4f]' % (opt.normalize, opt.normalize))
+def normalize_by_bbox_(mesh, bound):
+    """
+    Normalizes the mesh to [-bound, bound]^3.
+
+    Note that this is in-place.
+    """
+    print(f'[In simplemesh] --normalize specified, target bounding box is [-{bound:.4f}, {bound:.4f}]')
 
     bbox_min, bbox_max = mesh.bounds
     bbox_cent = .5 * (bbox_min + bbox_max)
@@ -34,35 +36,59 @@ if opt.normalize is not None:
     axis_extent = bbox_max - bbox_min
     max_extent = axis_extent.max()
 
-    scale_transfm = trimesh.transformations.scale_matrix(factor=2*opt.normalize/max_extent)
+    scale_transfm = trimesh.transformations.scale_matrix(factor=2*bound/max_extent)
     trans_transfm = trimesh.transformations.translation_matrix(direction=-bbox_cent)
 
     mesh.apply_transform(trans_transfm)
     mesh.apply_transform(scale_transfm)
 
-if opt.rotate is not None:
-    opt.rotate[1] = eval(opt.rotate[1])
-    
-    print('[HERE: In simplemesh] --rotate specified, will rotate %.4f degrees around the %s axis' % (opt.rotate[1], opt.rotate[0]))
-
-    if opt.rotate[0] == 'x':
+def rotate_by_deg_(mesh, axis, deg):
+    if axis == 'x':
         direction = [1,0,0]
-    elif opt.rotate[0] == 'y':
+    elif axis == 'y':
         direction = [0,1,0]
-    elif opt.rotate[0] == 'z':
+    elif axis == 'z':
         direction = [0,0,1]
     else:
-        print('Invalid rotation axis')
-        opt.rotate[1] = 0.
+        print(f'[In simplemesh] --rotate specified, but with invalid rotation axis: {axis}')
+        return
+        
+    print(f'[In simplemesh] --rotate specified, will rotate {deg:.4f} degrees around the {axis} axis')
 
     rot_transfm = trimesh.transformations.rotation_matrix(
-        angle=opt.rotate[1]*pi/180,
+        angle=deg*pi/180,
         direction=direction,
         point=[0,0,0])
     mesh.apply_transform(rot_transfm)
 
-if opt.output is None:
-    opt.output = opt.input[:-4] + '_aalnormed.obj'
+def export(mesh, in_name, out_name):
+    if out_name == True:
+        out_name = in_name[:-4] + '_processed.obj'
+    print(f'[In simplemesh] --output specified, will output to {out_name}')
+    mesh.export(out_name)
 
-#mesh.show()
-mesh.export(opt.output)
+if __name__ == '__main__':
+    mesh = trimesh.load(args.input)#, skip_textures=skip_textures)
+    
+    if args.info:
+        display_info(mesh)
+    
+    if args.rotate is not None:
+        i = 0
+        while i < len(args.rotate):
+            axis = args.rotate[i]
+            deg = eval(args.rotate[i+1])
+            rotate_by_deg_(mesh, axis, deg)
+            i = i + 2
+        del i, axis, deg
+
+    if args.normalize is not None:
+        bound = args.normalize
+        normalize_by_bbox_(mesh, bound)
+        del bound
+
+    if args.output is not None:
+        in_name = args.input
+        out_name = args.output
+        export(mesh, in_name, out_name)
+
